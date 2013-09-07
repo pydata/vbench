01069cb8636413038f81e9ca2b2876766bb60279
Yaroslav Halchenko <debian@onerussian.com>
2013-09-07 09:56:53 -0400

    ENH: vb_run_benchmarks -- use vbench's logging (if avail) and report time it took for a benchmark at debug level

--- vbench/scripts/vb_run_benchmarks.py
+++ vbench/scripts/vb_run_benchmarks.py
@@ -1,5 +1,14 @@
 import sys
 import cPickle as pickle
+import time
+
+import logging
+try:
+    from vbench.log import log
+except ImportError:
+    # if no pandas -- just a silent one
+    log = logging.getLogger('vb')
+    log.setLevel(logging.INFO)
 
 if len(sys.argv) != 3:
     print('Usage: script.py input output')
@@ -11,20 +20,22 @@ benchmarks = pickle.load(open(in_path))
 results = {}
 errors = 0
 for bmk in benchmarks:
+    t0 = time.time()
     try:
         res = bmk.run()
+        log.debug("Benchmark %s completed in %.2s sec" % (bmk, time.time()-t0))
     except Exception, e:
         errors += 1
-        print("E: Got an exception while running %s\n%s" % (bmk, e))
+        log.error("E: Got an exception while running %s\n%s" % (bmk, e))
         continue
 
     results[bmk.checksum] = res
 
     if not res['succeeded']:
         errors += 1
-        print("I: Failed to succeed with %s in stage %s."
-               % (bmk, res.get('stage', 'UNKNOWN')))
-        print(res.get('traceback', 'Traceback: UNKNOWN'))
+        log.warning("I: Failed to succeed with %s in stage %s."
+                    % (bmk, res.get('stage', 'UNKNOWN')))
+        log.info(res.get('traceback', 'Traceback: UNKNOWN'))
 
 benchmarks = pickle.dump(results, open(out_path, 'w'))
 sys.exit(errors)
